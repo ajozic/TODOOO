0f296871b7bf43290fa99b62f6eb9f42b67e2bca
aseparovic <antonio.separovic@thefalcontech.com>
2021-04-24 20:37:47 +0200

    login screen and dnd functionality

--- src/App.js
+++ src/App.js
@@ -1,88 +1,28 @@
 import React, { Component } from 'react'
-import Web3 from 'web3'
-import './App.css'
-import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
-import TodoList from './TodoList'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends Component {
-  componentWillMount() {
-    this.loadBlockchainData()
-  }
+import Login from './Login/Login'
+import TodoMain from "./Todo/TodoMain";
 
-  async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:75")
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
-    this.setState({ todoList })
-    const taskCount = await todoList.methods.taskCount().call()
-    this.setState({ taskCount })
-    for (var i = 1; i <= taskCount; i++) {
-      const task = await todoList.methods.tasks(i).call()
-      this.setState({
-        tasks: [...this.state.tasks, task]
-      })
-    }
-    this.setState({ loading: false })
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      taskCount: 0,
-      tasks: [],
-      loading: true
-    }
-
-    this.createTask = this.createTask.bind(this)
-    this.toggleCompleted = this.toggleCompleted.bind(this)
-  }
-
-  createTask(content) {
-    this.setState({ loading: true })
-    this.state.todoList.methods.createTask(content).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-      window.location.reload(false); 
-    })
-  }
-
-  toggleCompleted(taskId) {
-    this.setState({ loading: true })
-    this.state.todoList.methods.toggleCompleted(taskId).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#" target="_blank">Blockchain React Todo List</a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small><a className="nav-link" href="#"><span id="account"></span></a></small>
-            </li>
-          </ul>
-        </nav>
-        <div className="container-fluid">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex justify-content-center">
-              { this.state.loading
-                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <TodoList
-                  tasks={this.state.tasks}
-                  createTask={this.createTask}
-                  toggleCompleted={this.toggleCompleted} />  
-              } 
-            </main>
-          </div>
+const App = () => {
+  return(
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/todo">
+              <TodoMain />
+            </Route>
+          </Switch>
         </div>
-      </div>
-    );
-  }
+      </Router>
+  )
 }
-
 export default App;
